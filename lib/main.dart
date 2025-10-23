10868760a0a48e4ad6a9f924f049f886c723e3f2
putrireda <putriredamaulidia_2428240086@mhs.mdp.ac.id>
2025-10-23 16:22:00 +0700

    apdet

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:wisata_candi_mrizki_algipari/data/candi_data.dart';
-import 'package:wisata_candi_mrizki_algipari/screens/detail_screen.dart';
-import 'package:wisata_candi_mrizki_algipari/models/candi.dart';
+import 'package:aplikasi_candi_putriredamaulidia/data/candi_data.dart';
+import 'package:aplikasi_candi_putriredamaulidia/screens/detail_screen.dart';
+import 'package:aplikasi_candi_putriredamaulidia/models/candi.dart';
 
 void main() {
   runApp(const MyApp());
